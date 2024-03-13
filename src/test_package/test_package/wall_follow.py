2a5dc7d636dde5a3b73e8eefd3daef763df35ac3
Nathan Wolf-Sonkin <140115590+Nathan7577@users.noreply.github.com>
2024-03-13 11:53:02 -0400

    Robot now can follow straight walls

--- src/test_package/test_package/wall_follow.py
+++ src/test_package/test_package/wall_follow.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
+import numpy as np
 
 
 from geometry_msgs.msg import Twist
@@ -30,19 +31,35 @@ class Publisher(Node):
 
     def ir_callback(self, msg_in):
         # generates commands based on ir values
-        msg_out = Twist()
-        # IrIntensity[1] = side_left
-        # IrIntensity[2] = left
-        # IrIntensity[3] = front_left
-        # IrIntensity[4] = front_center_left
-        # IrIntensity[5] = front_center_right
-        # IrIntensity[6] = front_right
-        # IrIntensity[7] = right
-        print(msg_in.readings)
         
-        if msg_in.readings[1] >= 50:
+        # define output message type
+        msg_out = Twist() 
+        
+        # read in values from the IR sensors (type uint16)
+        side_left = msg_in.readings[0].value
+        left = msg_in.readings[1].value 
+        front_left = msg_in.readings[2].value
+        front_center_left = msg_in.readings[3].value
+        front_center_right = msg_in.readings[4].value
+        front_right = msg_in.readings[5].value
+        right = msg_in.readings[6].value
+        
+        # set distance threshold and typecast to uint16
+        threshold = np.uint16(50)
+
+        # Current controller is a bang bang controller which cannot handle turns.
+        # 
+        # Potential solution: implement a regulation controller based on left sensor data to keep sensor data
+        # near a set point. If the data goes outside of specified threshold, robot stops motion and starts
+        # rotating counter-clockwise until data is recieved again. 
+        #
+        # Potential solution 2: specifically set turn logic and wall follow logic sepratley and then assess the 
+        # situation based on sensor data. then follow protocall for the given situation.
+
+        # state machine logic for wall following
+        if side_left >= threshold:
             print('WALL DETECTED')
-            msg_out.linear.x = 0.0
+            msg_out.linear.x = 0.25
             msg_out.linear.y = 0.0
             msg_out.linear.z = 0.0
             msg_out.angular.x = 0.0
@@ -50,13 +67,14 @@ class Publisher(Node):
             msg_out.angular.z = -0.5
         else:
             print('NOTHING DETECTED')
-            msg_out.linear.x = 0.0
+            msg_out.linear.x = 0.5
             msg_out.linear.y = 0.0
             msg_out.linear.z = 0.0
             msg_out.angular.x = 0.0
             msg_out.angular.y = 0.0
             msg_out.angular.z = 0.5
 
+        # publish message
         self.publisher_.publish(msg_out)
 
 def main(args=None):
